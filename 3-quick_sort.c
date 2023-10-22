48425ae29450f3da10ae7548c7d3a0b0e9f62628
Kali <effahfrancis210@gmail.com>
2023-10-22 18:52:27 -0400

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,9 +13,10 @@ void quick_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	pivot_index = lomuto_part(array, size, 0, size - 1);
 	print_array(array, size);
-
+	pivot_index = lomuto_part(array, size, 0, size - 1);
+	/**print_array(array, size);
+	*/
 	quick_sort(array, pivot_index);
 	quick_sort(array + pivot_index + 1, size - pivot_index - 1);
 }
@@ -40,6 +41,7 @@ int lomuto_part(int *array, size_t size, int min, int max)
 
 	for (; i <= max - 1; i++)
 	{
+
 		if (array[i] < pivot)
 		{
 			j++;
