deb240261aafe8377dfa03b71afe2ccb253a7f1a
Kali <effahfrancis210@gmail.com>
2023-10-22 15:10:56 -0400

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 		return;
 
 	current = (*list)->next;
-	while(current)
+	while (current)
 	{
 		temp = current->prev;
 		while (temp && temp->n > current->n)
